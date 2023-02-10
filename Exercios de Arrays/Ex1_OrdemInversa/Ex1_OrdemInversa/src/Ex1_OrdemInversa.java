de8440388518f1f892e6824a3e3723c12dec95cc
celsojuniorladislau <celsojuniorladislau@gmail.com>
2023-02-10 07:39:51 -0300

    exercicios com Arrays (ordem inversa)

---
+++ Exercios de Arrays/Ex1_OrdemInversa/Ex1_OrdemInversa/src/Ex1_OrdemInversa.java
@@ -0,0 +1,22 @@
+public class Ex1_OrdemInversa {
+    public static void main(String[] args) {
+        
+        int[] vetor = {54, -10, 60, 500, 9, 7};
+    
+        //System.out.println(vetor.length);
+
+        System.out.print("Vetor: ");
+       int count = 0;
+        while(count < (vetor.length)) {
+            System.out.print(vetor[count] + " ");
+            count++;
+        } 
+        
+        System.out.print("\nVetor: ");
+        for(int i = vetor.length - 1; i >= 0; i--) {
+            System.out.print(vetor[i] + " ");
+
+        }
+    }   
+
+}
